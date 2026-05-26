2b9014a30a5958ca04de4635eb7c13e6dbdb7aa8
Abdulkarim Lahmuni <abdulkarim1422.18@gmail.com>
2026-05-26 13:14:50 +0300

    add federation plugin configuration to vite-app-1 and vite-app-2

--- vite-app-2/vite.config.ts
+++ vite-app-2/vite.config.ts
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import federation from "@originjs/vite-plugin-federation";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), federation({
+    name: "remoteApp2",
+    filename: "remoteEntry.js",
+    exposes: {
+      "./Button": "./src/Button.tsx",
+    },
+    shared: ["react", "react-dom"],
+  })],
 })
