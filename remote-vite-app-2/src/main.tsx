601b9cd96ef7f7cf86914005fd323106c8960876
Abdulkarim Lahmuni <abdulkarim1422.18@gmail.com>
2026-05-27 01:05:56 +0300

    feat: wrap App component with BrowserRouter for remote entry under /mf/remote

--- remote-vite-app-2/src/main.tsx
+++ remote-vite-app-2/src/main.tsx
@@ -2,9 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    {/* Standalone dev/preview entry: remote is served under /mf/remote */}
+    <BrowserRouter basename="/mf/remote">
+      <App />
+    </BrowserRouter>
   </StrictMode>,
 )
